995c16794041fd6b8eddb8407e6941010d514e46
Sebastian Toulier <65046165+SebTou08@users.noreply.github.com>
2021-06-07 11:43:33 -0500

    feat: add app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card
+      class="mx-auto"
+      max-width="344"
+      outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          Seguidores de @{{fullName}}
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          <strong>Numero oficial de seguidores:</strong>{{followers}}
+        </v-list-item-title>
+        <v-list-item-subtitle>En esta parte puedes aumentar los seguidores con el boton </v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+          tile
+          size="80"
+          color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+          outlined
+          rounded
+          text
+          v-on:click="followUser"
+      >
+        Seguir
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    followers: 0,
+    user: {
+      id: 1,
+      username: '_sebTou',
+      firstName: 'sebastian',
+      lastname: 'toulier',
+      email: 'seb@gmail.com',
+      isAdmin: true
+    }
+  }),
+
+  watch: {
+    followers(newFollowerCount, oldFollowerCount){
+      if(oldFollowerCount< newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+
+  computed: {
+    fullName(){
+      return `${this.user.firstName} ${this.user.lastname}`
+    }
+  },
+
+  methods: {
+    followUser(){
+      this.followers++;
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+
+};
+</script>
